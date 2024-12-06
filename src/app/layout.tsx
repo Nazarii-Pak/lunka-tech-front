671824fcf16b3e30a87b743d20825dc5dccdaaa9
Nazarii Pak <nazarii.pak@pettersonapps.com>
2024-12-06 15:48:37 +0200

    Init commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.css'
+
+const geistSans = localFont({
+	src: './fonts/GeistVF.woff',
+	variable: '--font-geist-sans',
+	weight: '100 900',
+})
+const geistMono = localFont({
+	src: './fonts/GeistMonoVF.woff',
+	variable: '--font-geist-mono',
+	weight: '100 900',
+})
+
+export const metadata: Metadata = {
+	title: 'lunka tech',
+	description: 'lunka tech',
+}
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html lang='en'>
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<main>{children}</main>
+				<footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'></footer>
+			</body>
+		</html>
+	)
+}
